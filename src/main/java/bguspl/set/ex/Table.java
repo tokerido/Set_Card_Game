109b2cf972e7e1e304954ad030fc5756cb2bfee0
Ido Toker <idoto@post.bgu.ac.il>
2024-02-07 00:13:42 +0200

    first commit

---
+++ src/main/java/bguspl/set/ex/Table.java
@@ -0,0 +1,149 @@
+package bguspl.set.ex;
+
+import bguspl.set.Env;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * This class contains the data that is visible to the player.
+ *
+ * @inv slotToCard[x] == y iff cardToSlot[y] == x
+ */
+public class Table {
+
+    /**
+     * The game environment object.
+     */
+    private final Env env;
+
+    /**
+     * Mapping between a slot and the card placed in it (null if none).
+     */
+    protected final Integer[] slotToCard; // card per slot (if any)
+
+    /**
+     * Mapping between a card and the slot it is in (null if none).
+     */
+    protected final Integer[] cardToSlot; // slot per card (if any)
+
+    /**
+     * Constructor for testing.
+     *
+     * @param env        - the game environment objects.
+     * @param slotToCard - mapping between a slot and the card placed in it (null if none).
+     * @param cardToSlot - mapping between a card and the slot it is in (null if none).
+     */
+    public Table(Env env, Integer[] slotToCard, Integer[] cardToSlot) {
+
+        this.env = env;
+        this.slotToCard = slotToCard;
+        this.cardToSlot = cardToSlot;
+    }
+
+    /**
+     * Constructor for actual usage.
+     *
+     * @param env - the game environment objects.
+     */
+    public Table(Env env) {
+
+        this(env, new Integer[env.config.tableSize], new Integer[env.config.deckSize]);
+    }
+
+    /**
+     * This method prints all possible legal sets of cards that are currently on the table.
+     */
+    public void hints() {
+        List<Integer> deck = Arrays.stream(slotToCard).filter(Objects::nonNull).collect(Collectors.toList());
+        env.util.findSets(deck, Integer.MAX_VALUE).forEach(set -> {
+            StringBuilder sb = new StringBuilder().append("Hint: Set found: ");
+            List<Integer> slots = Arrays.stream(set).mapToObj(card -> cardToSlot[card]).sorted().collect(Collectors.toList());
+            int[][] features = env.util.cardsToFeatures(set);
+            System.out.println(sb.append("slots: ").append(slots).append(" features: ").append(Arrays.deepToString(features)));
+        });
+    }
+
+    /**
+     * Count the number of cards currently on the table.
+     *
+     * @return - the number of cards on the table.
+     */
+    public int countCards() {
+        int cards = 0;
+        for (Integer card : slotToCard)
+            if (card != null)
+                ++cards;
+        return cards;
+    }
+
+    /**
+     * Places a card on the table in a grid slot.
+     * @param card - the card id to place in the slot.
+     * @param slot - the slot in which the card should be placed.
+     *
+     * @post - the card placed is on the table, in the assigned slot.
+     */
+    public void placeCard(int card, int slot) {
+        try {
+            Thread.sleep(env.config.tableDelayMillis);
+        } catch (InterruptedException ignored) {}
+
+        //our code start here
+        cardToSlot[card] = slot;
+        slotToCard[slot] = card;
+
+        // TODO implement
+    }
+
+    /**
+     * Removes a card from a grid slot on the table.
+     * @param slot - the slot from which to remove the card.
+     */
+    public void removeCard(int slot) {
+        try {
+            Thread.sleep(env.config.tableDelayMillis);
+        } catch (InterruptedException ignored) {}
+
+        //our code start here
+        syncronize(this){ // check if it works
+            int card = slotToCard[slot];
+            cardToSlot[card] = null;
+            slotToCard[slot] = null;
+        }
+        
+        // TODO implement
+        
+    }
+
+    /**
+     * Places a player token on a grid slot.
+     * @param player - the player the token belongs to.
+     * @param slot   - the slot on which to place the token.
+     */
+    public void placeToken(int player, int slot) {
+        // here we should get the player with the id (int player), and place its token on the slot
+        //update the number of tokens
+        // if this is the third token of the player, send the set to the dealer for a check???
+
+
+        // TODO implement
+    }
+
+    /**
+     * Removes a token of a player from a grid slot.
+     * @param player - the player the token belongs to.
+     * @param slot   - the slot from which to remove the token.
+     * @return       - true iff a token was successfully removed.
+     */
+    public boolean removeToken(int player, int slot) {
+        //delete token from slot(slot)
+        //update the number of tokens
+        //return true if token was removed, else return false
+
+        // TODO implement
+        return false;
+    }
+}
